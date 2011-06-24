84e06c1a859ff356a7b5cea542cf11ccaf64dbb8
atti84it <atti84it@yahoo.it>
2011-06-23 18:31:33 -0600

    Better Thread views (listing and details).

--- protected/views/thread/_view.php
+++ protected/views/thread/_view.php
@@ -1,19 +1,11 @@
 <div class="view">
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
-	<br />
+	<b><?php echo CHtml::link(CHtml::encode($data->title), array('view', 'id'=>$data->id)); ?></b><br />
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('title')); ?>:</b>
-	<?php echo CHtml::encode($data->title); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('short_desc')); ?>:</b>
 	<?php echo CHtml::encode($data->short_desc); ?>
 	<br />
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('desc')); ?>:</b>
-	<?php echo CHtml::encode($data->desc); ?>
+	<?php echo CHtml::encode(substr($data->desc,0,60)); ?> ...<?php echo CHtml::link('(read more)', array('view', 'id'=>$data->id)); ?>
 	<br />
 
 
--- protected/views/thread/view.php
+++ protected/views/thread/view.php
@@ -13,14 +13,8 @@ $this->menu=array(
 );
 ?>
 
-<h1>View Thread #<?php echo $model->id; ?></h1>
+<h1><?php echo CHtml::encode($model->title); ?></h1>
 
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'id',
-		'title',
-		'short_desc',
-		'desc',
-	),
-)); ?>
+<p><?php echo CHtml::encode($model->short_desc); ?></p>
+
+<?php echo CHtml::encode($model->desc); ?>
